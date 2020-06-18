2e5d117daea618907b4db6d633de0f014af212bb
pythonix <pythonix@yahoo.com>
2020-06-17 22:06:42 -0400

    add monitoring policies for aws resources

--- terraform/vpc-public-subnet/main.tf
+++ terraform/vpc-public-subnet/main.tf
@@ -59,7 +59,7 @@ resource "aws_subnet" "subnet_az1" {
 resource "aws_subnet" "subnet_az2" {
   vpc_id                  = "${aws_vpc.create_vpc.id}"
   cidr_block              = "${var.public_cidr_zone2}"
-  availability_zone = "${var.aws_region}b"
+  availability_zone = "${var.aws_region}c"
   map_public_ip_on_launch = true
   tags = {
   	Name =  "${var.aws_vpc_name}-subnet2"
